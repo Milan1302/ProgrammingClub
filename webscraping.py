b36e1a06ecd88bf21241404f596e5b7b20002767
Milan1302 <103596609+Milan1302@users.noreply.github.com>
2022-08-16 21:19:41 +0530

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -10,13 +10,9 @@ with open("data.json","w") as f:
 
 def write_json(new_data, filename='data.json'):
     with open(filename,'r+') as file:
-          # First we load existing data into a dict.
         file_data = json.load(file)
-        # Join new_data with file_data inside emp_details
         file_data.append(new_data)
-        # Sets file's current position at offset.
         file.seek(0)
-        # convert back to json.
         json.dump(file_data, file, indent = 4)
 count=0;
 driver = webdriver.Chrome(executable_path="C:\\Users\\rasto\\Downloads\\chromedriver_win32\\chromedriver.exe")
